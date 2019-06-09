c95270b8494886b912bf64389fa4bf40c4053478
Ryan1729 <Ryan1729@gmail.com>
2019-06-09 10:56:34 -0600

    write another benchmark that suggests that rendering no highlight takes 16.6 ms

--- libs/platform_layer/libs/opengl/benches/rendering.rs
+++ libs/platform_layer/libs/opengl/benches/rendering.rs
@@ -23,7 +23,7 @@ fn render_buffer_view_benchmark(c: &mut Criterion) {
                 let glyph_brush: GlyphBrush<Vertex> = get_glyph_brush(&font_info);
                 (glyph_brush, full_highlight_view, font_info)
             },
-            |(mut glyph_brush, view, font_info)| {
+            |(glyph_brush, view, font_info)| {
                 render_buffer_view(&mut black_box(glyph_brush), &view, &font_info)
             },
             BatchSize::LargeInput,
@@ -31,55 +31,103 @@ fn render_buffer_view_benchmark(c: &mut Criterion) {
     });
 }
 
+const SCREEN_SIZE: u32 = 2048;
+
+fn render_frame(
+    (gl_state, glyph_brush, window, extras): (
+        &mut gl_layer::State,
+        &mut GlyphBrush<Vertex>,
+        &mut glutin::WindowedContext,
+        RenderExtras,
+    ),
+) {
+    gl_layer::render(
+        black_box(gl_state),
+        black_box(glyph_brush),
+        SCREEN_SIZE as _,
+        SCREEN_SIZE as _,
+        extras.clone(),
+    )
+    .unwrap();
+    window.swap_buffers().unwrap();
+}
+
+fn get_windowed_context() -> glutin::WindowedContext {
+    let window = glutin::WindowedContext::new_windowed(
+        glutin::WindowBuilder::new()
+            .with_dimensions((SCREEN_SIZE, SCREEN_SIZE).into())
+            .with_title(SCREEN_SIZE.to_string()),
+        glutin::ContextBuilder::new()
+            .with_gl_profile(GlProfile::Core)
+            .with_gl(GlRequest::Specific(Api::OpenGl, (3, 2)))
+            .with_srgb(true),
+        &glutin::EventsLoop::new(),
+    )
+    .unwrap();
+    unsafe { window.make_current() }.unwrap();
+    window
+}
+
 fn gl_layer_benchmark(c: &mut Criterion) {
-    const SCREEN_SIZE: u32 = 2048;
-
-    fn render_frame(
-        (gl_state, glyph_brush, window, extras): (
-            &mut gl_layer::State,
-            &mut GlyphBrush<Vertex>,
-            &mut glutin::WindowedContext,
-            RenderExtras,
-        ),
-    ) {
-        gl_layer::render(
-            black_box(gl_state),
-            black_box(glyph_brush),
-            2048 as _,
-            2048 as _,
-            extras.clone(),
-        )
-        .unwrap();
-        window.swap_buffers().unwrap();
-    };
-
-    c.bench_function("gl_layer full highlight", |b| {
+    c.bench_function("gl_layer no highlight", |b| {
         b.iter_batched(
             || {
-                let mut buffer = slipsum_buffer();
-                let (full_highlight_view, _) = update_and_render(
-                    &mut buffer,
-                    Input::ExtendSelectionForAllCursors(Move::ToBufferEnd),
-                );
                 let font_info = FontInfo::new(2.0).unwrap();
 
-                let mut window = glutin::WindowedContext::new_windowed(
-                    glutin::WindowBuilder::new()
-                        .with_dimensions((SCREEN_SIZE, SCREEN_SIZE).into())
-                        .with_title(SCREEN_SIZE.to_string()),
-                    glutin::ContextBuilder::new()
-                        .with_gl_profile(GlProfile::Core)
-                        .with_gl(GlRequest::Specific(Api::OpenGl, (3, 2)))
-                        .with_srgb(true),
-                    &glutin::EventsLoop::new(),
-                )
-                .unwrap();
-                unsafe { window.make_current() }.unwrap();
+                let mut window = get_windowed_context();
+
                 let mut glyph_brush: GlyphBrush<Vertex> = get_glyph_brush(&font_info);
 
                 let mut gl_state =
                     gl_layer::init(&glyph_brush, |symbol| window.get_proc_address(symbol) as _)
                         .unwrap();
+
+                let mut buffer = slipsum_buffer();
+
+                {
+                    let (full_highlight_view, _) = update_and_render(
+                        &mut buffer,
+                        Input::ExtendSelectionForAllCursors(Move::ToBufferEnd),
+                    );
+
+                    let extras =
+                        render_buffer_view(&mut glyph_brush, &full_highlight_view, &font_info);
+
+                    // fill the glyph cache
+                    render_frame((&mut gl_state, &mut glyph_brush, &mut window, extras))
+                }
+
+                let (no_highlight_view, _) =
+                    update_and_render(&mut buffer, Input::MoveAllCursors(Move::ToBufferStart));
+                let extras = render_buffer_view(&mut glyph_brush, &no_highlight_view, &font_info);
+
+                (gl_state, glyph_brush, window, extras)
+            },
+            |(mut gl_state, mut glyph_brush, mut window, extras)| {
+                render_frame((&mut gl_state, &mut glyph_brush, &mut window, extras))
+            },
+            BatchSize::LargeInput,
+        )
+    });
+
+    c.bench_function("gl_layer full highlight", |b| {
+        b.iter_batched(
+            || {
+                let font_info = FontInfo::new(2.0).unwrap();
+
+                let mut window = get_windowed_context();
+
+                let mut glyph_brush: GlyphBrush<Vertex> = get_glyph_brush(&font_info);
+
+                let mut gl_state =
+                    gl_layer::init(&glyph_brush, |symbol| window.get_proc_address(symbol) as _)
+                        .unwrap();
+
+                let mut buffer = slipsum_buffer();
+                let (full_highlight_view, _) = update_and_render(
+                    &mut buffer,
+                    Input::ExtendSelectionForAllCursors(Move::ToBufferEnd),
+                );
 
                 {
                     let extras =
